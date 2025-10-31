ca7f3c1863e34836636a564d8a6e0dfa1c36fc68
N-garai <betatesterpro30@gmail.com>
2025-10-31 09:11:41 +0530

    cleanup: remove unnecessary files and update gitignore

--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .appwrite_client import account, databases, storage
-from .auth import login_required, role_required, admin_required
-
-__all__ = ['account','databases','storage','login_required','role_required','admin_required']
